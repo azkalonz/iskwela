0c7f708cf33008fbc106440633cd7bde1d9a7e7a
Jayson Barino <barinojayson@gmail.com>
2020-05-16 21:02:21 +0800

    user migration and seed

--- database/seeds/artifacts/users.sql
+++ database/seeds/artifacts/users.sql
@@ -1,19 +1,17 @@
-/*Data for the table `users` */
-
-insert  into `users`(`id`,`username`,`user_type`,`password`,`name`,`deleted_at`) values 
-(1,'jayson','s','$2y$10$jucHJFtsTUhgcPnmXcsO7OfFX2onnYgHRoQ9Qp5t4AWdNgiUzoaVC','jayson',NULL),
-(2,'grace','s','$2y$10$Wu92BpaszK6FJ0HUDT3kM.JjTrxCHFIeQ.sMFrUjIDb4PQk3eEvC2','grace',NULL),
-(3,'jen','s','$2y$10$e0N0SoMTdhT6yQCap3ci1u6oXUKwteQx5YktKQ/.jgSSbTFPuKNHC','jen',NULL),
-(4,'davy','s','$2y$10$BNe9fQp8f9tc1US95ES01OdA2PMRA2Rz6isSNAq8ygsBmG.YVxdZ.','davy',NULL),
-(5,'jacque','s','$2y$10$LkeSr.HGx45pp5RN3hZG5uhpCEXQ/cUiqr7wbPNHwgJAWEcFPbrhS','jacque',NULL),
-(6,'dhame','s','$2y$10$3qTGK4T4mwpe5OCW48Z4c.Y9eFdZ6SalKXpkwoCHWyWVUu1GPpYrm','dhame',NULL),
-(7,'vhen','s','$2y$10$4Nx9QHCZ4d4oS.SZqZuzeeSaZcsBzcksnEh.yCWPQZV0f7e0td6CK','vhen',NULL),
-(8,'ttom','t','$2y$10$RH2LNGmb8SCvRlCmm2ahBOshN33xTG4Lo0I/6aALlP.MhhQFSg2Py','teacher tom',NULL),
-(9,'tjayson','t','$2y$10$jucHJFtsTUhgcPnmXcsO7OfFX2onnYgHRoQ9Qp5t4AWdNgiUzoaVC','teacher jayson',NULL),
-(10,'tgrace','t','$2y$10$Wu92BpaszK6FJ0HUDT3kM.JjTrxCHFIeQ.sMFrUjIDb4PQk3eEvC2','teacher grace',NULL),
-(11,'tjen','t','$2y$10$e0N0SoMTdhT6yQCap3ci1u6oXUKwteQx5YktKQ/.jgSSbTFPuKNHC','teacher jen',NULL),
-(12,'tdavy','t','$2y$10$BNe9fQp8f9tc1US95ES01OdA2PMRA2Rz6isSNAq8ygsBmG.YVxdZ.','teacher davy',NULL),
-(13,'tjacque','t','$2y$10$LkeSr.HGx45pp5RN3hZG5uhpCEXQ/cUiqr7wbPNHwgJAWEcFPbrhS','teacher jacque',NULL),
-(14,'tdhame','t','$2y$10$3qTGK4T4mwpe5OCW48Z4c.Y9eFdZ6SalKXpkwoCHWyWVUu1GPpYrm','teacher dhame',NULL),
-(15,'tvhen','t','$2y$10$4Nx9QHCZ4d4oS.SZqZuzeeSaZcsBzcksnEh.yCWPQZV0f7e0td6CK','teacher vhen',NULL),
-(16,'tom','s','$2y$10$RH2LNGmb8SCvRlCmm2ahBOshN33xTG4Lo0I/6aALlP.MhhQFSg2Py','tom',NULL);
+insert into `users` (`id`, `username`, `user_type`, `password`, `first_name`, `last_name`, `gender`, `email`, `phone_number`, `status`) values 
+('1','jayson','s','$2y$10$jucHJFtsTUhgcPnmXcsO7OfFX2onnYgHRoQ9Qp5t4AWdNgiUzoaVC','jayson','barino','m','barinojayson@gmail.con','111','1'),
+('2','grace','s','$2y$10$Wu92BpaszK6FJ0HUDT3kM.JjTrxCHFIeQ.sMFrUjIDb4PQk3eEvC2','grace','ungui','f','xxx@gamil.com','111','1'),
+('3','jen','s','$2y$10$e0N0SoMTdhT6yQCap3ci1u6oXUKwteQx5YktKQ/.jgSSbTFPuKNHC','jen','castillo','f','xxx@gamil.com','111','1'),
+('4','davy','s','$2y$10$BNe9fQp8f9tc1US95ES01OdA2PMRA2Rz6isSNAq8ygsBmG.YVxdZ.','davy','castillo','m','xxx@gamil.com','111','1'),
+('5','jacque','s','$2y$10$LkeSr.HGx45pp5RN3hZG5uhpCEXQ/cUiqr7wbPNHwgJAWEcFPbrhS','jacque','amaya','f','xxx@gamil.com','111','1'),
+('6','dhame','s','$2y$10$3qTGK4T4mwpe5OCW48Z4c.Y9eFdZ6SalKXpkwoCHWyWVUu1GPpYrm','dhame','amaya','f','xxx@gamil.com','111','1'),
+('7','vhen','s','$2y$10$4Nx9QHCZ4d4oS.SZqZuzeeSaZcsBzcksnEh.yCWPQZV0f7e0td6CK','vhen','fernandez','m','xxx@gamil.com','111','1'),
+('8','ttom','t','$2y$10$RH2LNGmb8SCvRlCmm2ahBOshN33xTG4Lo0I/6aALlP.MhhQFSg2Py','teacher tom','cruz','m','xxx@gamil.com','111','1'),
+('9','tjayson','t','$2y$10$jucHJFtsTUhgcPnmXcsO7OfFX2onnYgHRoQ9Qp5t4AWdNgiUzoaVC','teacher jayson','barino','m','xxx@gamil.com','111','1'),
+('10','tgrace','t','$2y$10$Wu92BpaszK6FJ0HUDT3kM.JjTrxCHFIeQ.sMFrUjIDb4PQk3eEvC2','teacher grace','ungui','f','xxx@gamil.com','111','1'),
+('11','tjen','t','$2y$10$e0N0SoMTdhT6yQCap3ci1u6oXUKwteQx5YktKQ/.jgSSbTFPuKNHC','teacher jen','amaya','f','xxx@gamil.com','111','1'),
+('12','tdavy','t','$2y$10$BNe9fQp8f9tc1US95ES01OdA2PMRA2Rz6isSNAq8ygsBmG.YVxdZ.','teacher davy','castillo','m','xxx@gamil.com','111','1'),
+('13','tjacque','t','$2y$10$LkeSr.HGx45pp5RN3hZG5uhpCEXQ/cUiqr7wbPNHwgJAWEcFPbrhS','teacher jacque','amaya','f','xxx@gamil.com','111','1'),
+('14','tdhame','t','$2y$10$3qTGK4T4mwpe5OCW48Z4c.Y9eFdZ6SalKXpkwoCHWyWVUu1GPpYrm','teacher dhame','amaya','f','xxx@gamil.com','111','1'),
+('15','tvhen','t','$2y$10$4Nx9QHCZ4d4oS.SZqZuzeeSaZcsBzcksnEh.yCWPQZV0f7e0td6CK','teacher vhen','fernandez','m','xxx@gamil.com','111','1'),
+('16','tom','s','$2y$10$RH2LNGmb8SCvRlCmm2ahBOshN33xTG4Lo0I/6aALlP.MhhQFSg2Py','tom','cruz','m','xxx@gamil.com','111','1');
