c7d0170c38c512ef0ab4ea1c78ea5ace564d7dcf
Vhen Joseph <vhenjosephbiz@gmail.com>
2020-05-30 18:02:09 +0800

    feat(import): update users import password

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -50,7 +50,7 @@ class UsersImport implements ToModel, WithStartRow, WithCalculatedFormulas
             'phone_number'  => $row[4],
             'email'         => $row[5],
             'user_type'     => $this::USER_TYPE,
-            'password'      => Hash::make(str_replace(' ', '', strtolower($row[2]))),
+            'password'      => strtolower($row[2]),
             'school_id'     => $this->school->id
         ]);
     }
