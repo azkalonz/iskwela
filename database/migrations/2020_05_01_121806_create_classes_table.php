782125197e1216831dee21df546fae7b583c64cc
Vhen Joseph <vhenjosephbiz@gmail.com>
2020-06-02 22:47:22 +0800

    fix(migration): revert frequency column

--- database/migrations/2020_05_01_121806_create_classes_table.php
+++ database/migrations/2020_05_01_121806_create_classes_table.php
@@ -30,6 +30,7 @@ class CreateClassesTable extends Migration
             $table->date('date_to')->nullable();
             $table->time('time_from');
             $table->time('time_to');
+            $table->string('frequency', 50);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             $table->softDeletes();
