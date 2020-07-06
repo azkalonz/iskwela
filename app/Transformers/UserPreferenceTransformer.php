b3faae35df96f55c6f7e90b411d575ec977ed5b4
Jayson Grey <barinojayson@gmail.com>
2020-07-06 17:21:39 +0800

    fix(user): user preference - profile picture

--- app/Transformers/UserPreferenceTransformer.php
+++ app/Transformers/UserPreferenceTransformer.php
@@ -9,7 +9,7 @@ class UserPreferenceTransformer extends TransformerAbstract
     public function transform(\App\Models\UserPreference $user_preference)
     {
 		return [
-			'profile_picture' => ($user_preference->profile_picture) ? sprintf('%s/api/download/user/profile-picture', env('APP_URL')) : "",
+			'profile_picture' => $user_preference->profile_picture,
 			'push_notification' => $user_preference->push_notification,
 			'email_subscription' => $user_preference->email_subscription
 		];
