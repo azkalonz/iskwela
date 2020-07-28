5f2d9621467146cae28178394a0870acb6fb1c9f
Vhen Joseph <vhenjosephbiz@gmail.com>
2020-07-28 17:39:44 +0800

    fix(posts): update foreign key of comment to use post_id instead

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -28,13 +28,13 @@ class Comment extends Model
         return $this->belongsTo(User::class, 'created_by');
     }
 
-    /**
-     * The has Many Relationship
-     *
-     * @var array
-     */
-    public function replies()
-    {
-        return $this->hasMany(Comment::class, 'parent_id');
-    }
+    // /**
+    //  * The has Many Relationship
+    //  *
+    //  * @var array
+    //  */
+    // public function replies()
+    // {
+    //     return $this->hasMany(Comment::class, 'parent_id');
+    // }
 }
